73ca565365d93baf4e34b666b876746cfd13cd72
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-07-26 23:14:48 +0100

    Reduce pull reading

--- NucInstDigApp/src/NucInstDig.cpp
+++ NucInstDigApp/src/NucInstDig.cpp
@@ -469,25 +469,15 @@ asynStatus NucInstDig::writeOctet(asynUser *pasynUser, const char *value, size_t
 void NucInstDig::updateTraces()
 {
     static const double TRACE_UPDATE_TIME = atof(getenv("TRACE_UPDATE_TIME") != NULL ? getenv("TRACE_UPDATE_TIME") : "1.0");
-    static const double TRACE_DELAY_TIME = atof(getenv("TRACE_DELAY_TIME") != NULL ? getenv("TRACE_DELAY_TIME") : "0.0");
-    epicsTimeStamp last_update, now;
-    epicsTimeGetCurrent(&last_update);
     while(true)
     {
+        epicsThreadSleep(TRACE_UPDATE_TIME);
         try {
             zmq::message_t reply{};
             zmq::recv_result_t nbytes = m_zmq_stream.recv(reply, zmq::recv_flags::none);
             if (!nbytes || *nbytes == 0)
             {
                 epicsThreadSleep(0.5);
-                continue;
-            }
-            epicsTimeGetCurrent(&now);
-            if (epicsTimeDiffInSeconds(&now, &last_update) < TRACE_UPDATE_TIME)
-            {
-                if (TRACE_DELAY_TIME != 0.0) {
-                    epicsThreadSleep(TRACE_DELAY_TIME);
-                }
                 continue;
             }
             auto msg = GetDigitizerAnalogTraceMessage(reply.data());
@@ -523,7 +513,6 @@ void NucInstDig::updateTraces()
                     doCallbacksFloat64Array(reinterpret_cast<epicsFloat64*>(m_traceY[j].data()), m_traceY[j].size(), P_traceY[j], 0);
                 }
             }
-            epicsTimeGetCurrent(&last_update);
         }
         catch(const std::exception& ex)
         {
